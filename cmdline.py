a3b76700443cc2ea1c5e23cafcdfd6ba45cc1bb8
Adaptee <adaptee@gmail.com>
2010-11-24 22:47:38 +0800

    copy and split mmacman

---
+++ cmdline.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+
+from optparse import OptionParser, OptionGroup
+
+parser = OptionParser()
+group = OptionGroup(parser, "General Options",
+                    "Can be used standalone and in combination with any other options")
+group.add_option("-y", "--update", dest="update", action="store_true",
+                 help="Update all Databases (usable standalone, or in combination with everything)")
+group.add_option("-c", "--configfile", dest="configfile", metavar="FILE", default="/etc/pyalpmm.conf",
+                 help="use given file as a config file")
+parser.add_option_group(group)
+
+group = OptionGroup(parser, "Additional flags", "To be used in combination with other actions")
+group.add_option("-f", "--force", dest="force", action="store_true",
+                 help="force action")
+group.add_option("-d", "--nodeps", dest="nodeps", action="store_true",
+                 help="ignore dependencies")
+group.add_option("-w", "--downloadonly", dest="download_only", action="store_true",
+                 help="only download packages")
+group.add_option("-i", "--info", dest="info", action="store_true",
+                 help="Get info for some package. With -Q local and with -S from sync repo")
+group.add_option("", "--no-confirm", dest="confirm", action="store_false",
+                 help="Never ask the user for confirmation", default=True)
+# no transparency not implemented ...
+group.add_option("", "--no-transparency", dest="transparency",
+                 action="store_false", default=True,
+                 help="Do not try to transparently handle the AUR " \
+                      "(cannnot be switched off atm, so this option does nil)")
+
+parser.add_option_group(group)
+
+group = OptionGroup(parser, "Sync Actions",
+                    "-S activates the sync actions")
+group.add_option("-S", "--sync", dest="sync", action="store_true",
+                 help="Synchronise package")
+group.add_option("-u", "--sysupgrade", dest="sysupgrade", action="store_true",
+                 help="Perform a global system upgrade")
+group.add_option("-s", "--search", dest="search", action="store_true",
+                 help="Search package in SyncDatabases")
+parser.add_option_group(group)
+
+group = OptionGroup(parser, "Upgrade Actions",
+                    "-U activates the upgrade actions")
+group.add_option("-U", "--upgrade", dest="upgrade", action="store_true",
+                 help="Upgrade package")
+parser.add_option_group(group)
+
+group = OptionGroup(parser, "Query Actions",
+                    "-Q activates the Query Actions")
+group.add_option("-Q", "--query", dest="query", action="store_true",
+                 help="List all local packages")
+group.add_option("", "--aur", dest="aur", action="store_true",
+                 help="While listing all packages, check which ones are " \
+                      "from AUR. More precisly: which ones are not in the" \
+                      "regular repositories." )
+group.add_option("", "--orphan", dest="orphan", action="store_true",
+                 help="Search for 'orphan' packages, means not explicitly " \
+                 "installed packages, which are not required by any " \
+                 "other installed package")
+group.add_option("-F", "--files", dest="show_files", action="store_true",
+                 help="List of all files in the given package")
+group.add_option("-o", "--owns", dest="owns", action="store_true",
+                 help="Look for package that contains the given file/dir/path")
+group.add_option("-g", "--groups", dest="groups", action="store_true",
+                 help="List all groups available in the sync repository")
+
+parser.add_option_group(group)
+
+group = OptionGroup(parser, "Remove Actions",
+                    "-R activates the Remove Actions")
+group.add_option("-R", "--remove", dest="remove", action="store_true",
+                 help="Remove the given packages from the system")
+group.add_option("", "--no-recursive", dest="recursive_remove",
+                 default="True", action="store_false",
+                 help="Do not recursivly remove unneeded dependencies on remove")
+parser.add_option_group(group)
+
+group = OptionGroup(parser, "Build Actions",
+                    "-B activates the Build Actions")
+group.add_option("-B", "--build", dest="build", action="store_true",
+                 help="Build the given packages either from abs or from aur")
+group.add_option("-I", "--install", dest="build_install", action="store_true",
+                 help="Install the built package")
+group.add_option("-e", "--edit", dest="build_edit", action="store_true",
+                 help="Edit the PKGBUILD before building")
+group.add_option("", "--no-cleanup", dest="build_cleanup", action="store_false",
+                 default="True",
+                 help="Don't cleanup (delete) the build dir before")
+group.add_option("", "--no-prepare", dest="build_prepare", action="store_false",
+                 default="True",
+                 help="Don't prepare (download scripts and sources) the build")
+parser.add_option_group(group)
+
+group = OptionGroup(parser, "Evil Options",
+                    "only --args are used in the evil opts")
+group.add_option("", "--clear-pkg-cache", dest="cpc", action="store_true",
+                 help="cleanup the package cache directory")
+group.add_option("", "--create-default-config", dest="create_config", action="store_true",
+                 help="write the default configuration to a file")
+
+parser.add_option_group(group)
---
+++ myevents.py
@@ -0,0 +1,247 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+
+import sys
+import pyalpmm_raw as p
+
+from pyalpmm import Events
+from pyalpmm.tools import ProgressBar, FancySize
+
+class ArchmanEvents(Events):
+    def __init__(self):
+        super(ArchmanEvents, self).__init__()
+        self.progress_obj = None
+        self.processed_pkgs = None
+        self.active_pkg = None
+        self.last_pkg = None
+        self.line_dirty = False
+        self.last_total_download = None
+        self.want_user_confirm = True
+
+
+    def _clean_line(self, cleanup_text="\n"):
+        if self.line_dirty:
+            sys.stdout.write(cleanup_text)
+            sys.stdout.flush()
+            self.line_dirty = False
+
+    def DatabaseUpToDate(self, **kw):
+        print "[i] Database up to date: {0}".format(kw["repo"])
+
+    def DatabaseUpdated(self, **kw):
+        if self.progress_obj:
+            self._clean_line("\r{0}\n".format(self.progress_obj.step_to_end()))
+        print "[+] Database updated: {0}".format(kw["repo"])
+
+    def DatabaseUpdateError(self, **kw):
+        print "[e] Database could not be updated: {0}".format(kw["repo"])
+
+    def StartResolvingDependencies(self):
+        print "[i] Resolving Dependencies..."
+
+    def StartCheckingInterConflicts(self):
+        print "[i] Checking Inter-Conflicts..."
+
+    def StartRetrievingPackages(self, **kw):
+        self._clean_line()
+        print "[+] Retrieving from {0}".format(kw["repo"])
+
+    def StartUpgradingPackage(self, **kw):
+        self._clean_line()
+        self.progress_obj = None
+        self.active_pkg = kw["pkg"]
+
+    def StartRemovingPackage(self, **kw):
+        self._clean_line()
+        self.progress_obj = None
+        self.active_pkg = kw["pkg"]
+
+    def StartInstallingPackage(self, **kw):
+        self._clean_line()
+        self.progress_obj = None
+        self.active_pkg = kw["pkg"]
+
+    def DoneInstallingPackage(self, **kw):
+        self._clean_line()
+
+    def DoneRemovingPackage(self, **kw):
+        self._clean_line()
+
+    def DoneUpgradingPackage(self, **kw):
+        self._clean_line()
+
+    def StartCheckingPackageIntegrity(self):
+        print "[i] Checking package integrity..."
+
+    def StartCheckingFileConflicts(self):
+        self.progress_obj = None
+        pass
+
+    def StartPreAURTransaction(self, **kw):
+        print
+        print "[i] The following packages are installed from regular repos"
+        print "[i] After this the following AUR packages will be built: {0}". \
+              format(", ".join(kw["aur_targets"]))
+        print
+
+    def StartBuild(self, **kw):
+        print "[+] Starting build: {0.name}-{0.version}".format(kw["pkg"])
+
+    def ReInstallingPackage(self, **kw):
+        print "[i] Reinstalling package: {0.name}-{0.version}".format(kw["pkg"])
+
+    def _progress(self, operation, kw):
+        if self.progress_obj is None:
+            desc = "[i] {0} {1}".format(operation, kw["pkgname"])
+            self.progress_obj = ProgressBar(100, desc)
+
+        sys.stdout.write(self.progress_obj.step_to(kw["percent"]) + "\r")
+        sys.stdout.flush()
+
+        self.line_dirty = True
+
+    def StartNewDownload(self, **kw):
+        fn = filename = kw["filename"]
+
+        self._clean_line()
+
+        self.active_pkg = None
+        # first catch db-info downloads
+        if fn.endswith(".db.tar.gz"):
+            self.active_pkg = fn
+            size = None
+            label = "[D] {0}".format(fn)
+        # here a package is downloaded
+        else:
+            for name, pkg in self.processed_pkgs.items():
+                if fn.find(name) == 0:
+                    self.active_pkg = pkg
+            assert self.active_pkg is not None
+            size = int(self.active_pkg.size.raw)
+            label = "[D] {0.name}-{0.version}".format(self.active_pkg)
+
+
+        self.progress_obj = ProgressBar(size, label)
+
+    def ProgressDownload(self, **kw):
+        sys.stdout.write((self.progress_obj.step_to(kw["transfered"]) \
+                          if self.progress_obj.endvalue \
+                          else self.progress_obj.step_to(10)) + "\r")
+        sys.stdout.flush()
+
+        self.line_dirty = True
+
+    def ProgressDownloadTotal(self, **kw):
+        if self.last_total_download is None:
+            sys.stdout.write("\n")
+            sys.stdout.flush()
+
+        self.processed_pkgs = dict((p.name, p) for p in kw["pkgs"])
+
+    def ProgressInstall(self, **kw):
+        self._progress("Installing:", kw)
+
+    def ProgressRemove(self, **kw):
+        self._progress("Removing:", kw)
+
+    def ProgressUpgrade(self, **kw):
+        self._progress("Upgrading:", kw)
+
+    def ProgressConflict(self, **kw):
+        self._progress("Checking File Conflicts:", kw)
+
+    def ProcessingAURPackages(self, **kw):
+        print "[i] Building the following AUR packages now!"
+        for pkg in kw["add"]:
+            print "    [i] {0}".format(pkg.name)
+
+        print
+
+        if not self.want_user_confirm:
+            return
+
+        if AskUser("[?] Really continue (y/n)? ").answer == "n":
+            raise UserError("User aborted/declined operation!")
+
+    def ProcessingPackages(self, **kw):
+        isize, size, rsize = 0, 0, 0
+
+        print
+        for which in ["remove", "add"]:
+            if kw[which] is None or len(kw[which]) == 0:
+                continue
+
+            print {"remove": "[i] Removing the following packages:",
+                   "add": "[i] Adding/Upgrading the following packages:"}[which]
+            dashes = "-"*63
+            print "+" + dashes + "+"
+            for pkg in sorted(kw[which], key=lambda pkg: pkg.name):
+                isize_obj = pkg.isize.rebuild(force_suffix="MB")
+                size_obj = pkg.size.rebuild(force_suffix="MB")
+                if which == "add":
+                    isize += isize_obj.raw
+                    size += size_obj.raw
+                    show_size = "{0:>9} | {1:>9}".format(size_obj, isize_obj)
+                else:
+                    rsize += pkg.size.raw
+                    show_size = "{0:>9}".format(size_obj)
+
+                name = "| {0.name}-{0.version}".format(pkg)
+                print "{0:40}{1:>23} |".format(name, show_size)
+            print "+" + dashes + "+"
+
+        if kw["add"] is not None and len(kw["add"]) != 0:
+            print "[i] {0:38}{1:>9} | {2:>9}". \
+                  format("Total download | install size:",
+                         FancySize(size), FancySize(isize))
+
+        if kw["remove"] is not None and len(kw["remove"]) != 0:
+            print "[i] Total diskspace released:{0:>34}". \
+                  format(FancySize(rsize))
+
+        if not self.want_user_confirm:
+            return
+
+        if AskUser("[?] Really continue (y/n)? ").answer == "n":
+            raise UserError("User aborted/declined operation!")
+
+    # Errors
+    def PackageNotFound(self, **kw):
+        print "[e] {0}".format(kw["e"])
+
+    def UnsatisfiedDependencies(self, **kw):
+        print "[e] {0}".format(kw["e"])
+        for miss in kw["e"].data:
+            print ("[!]   '{0.target}' depends on '{0.causingpkg}', which "
+                   "is marked for removal").format(miss)
+
+    def FileConflictDetected(self, **kw):
+        print
+        print "[e] {0}".format(kw["e"])
+        info = kw["e"].data
+        for conflict in info:
+            if not conflict["target_pkg"]:
+                print ("[!]   '{0[local_pkg]}' conflicts with "
+                       "local file: '{0[file]}'").format(conflict)
+            else:
+                print ("[!]   '{0[target_pkg]}' and '{0[local_pkg]}' contain a "
+                       "conflicting file: '{0[file]}'").format(conflict)
+
+    def NothingToBeDone(self, **kw):
+        print "[e] {0}".format(kw["e"])
+
+    def NotRoot(self, **kw):
+        print "[e] {0}".format(kw["e"])
+        sys.exit()
+
+    def UserAbort(self, **kw):
+        print "[e] {0}".format(kw["e"])
+        sys.exit()
+
+    def BuildProblem(self, **kw):
+        print "[e] {0}".format(kw["e"])
+        sys.exit()
+
+    def ConflictingDependencies(self, **kw):
+        print "[e] {0}".format(kw["e"])
+        sys.exit()
